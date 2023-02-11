7b01a02ddf2c123c6bb6c6d352b098f733dc3b37
Nkorsah <Nkorsah21@gmail.com>
2023-02-11 14:19:19 -0500

    Update Covid-19 data.py

--- Covid-19 data.py
+++ Covid-19 data.py
@@ -1,1 +1,10 @@
 # Python Heatmap project
+import covidactnow
+
+api = covidactnow.User(api_key = '52946c7f007449fdb3f2f6d27620027e')
+
+washingtonInfectionRate = api.infRate('WA')
+massachussettsVaxRate = api.vaxRate('MA')
+
+print(f"{washingtonInfectionRate = }")
+print(f"{massachussettsVaxRate = }")
