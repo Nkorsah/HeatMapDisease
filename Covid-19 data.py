009e05beae1c233b5f98d93de36f66ed3336c541
Nkorsah <Nkorsah21@gmail.com>
2023-02-11 15:52:54 -0500

    Next

--- Covid-19 data.py
+++ Covid-19 data.py
@@ -1,13 +1,17 @@
 # Python Heatmap project
 # api key: 52946c7f007449fdb3f2f6d27620027e
+# Commands: https://apidocs.covidactnow.org/data-definitions
 
 import covidactnow
 
 api = covidactnow.User(api_key = '52946c7f007449fdb3f2f6d27620027e')
+
 
 PAInfectionRate = api.infRate('PA')
 massachussettsVaxRate = api.vaxRate('MA')
 
 print(f"{PAInfectionRate = }")
 print(f"{massachussettsVaxRate = }")
-print(api.actuals.cases)
+#print(actuals.cases)
+data = open('counties.csv', 'r')
+
