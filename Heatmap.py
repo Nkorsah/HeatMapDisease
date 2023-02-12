5f7c8b053c64075c9392b41ac8f9db4c6ed93e79
Nkorsah <Nkorsah21@gmail.com>
2023-02-11 20:34:14 -0500

    Yeo

--- Heatmap.py
+++ Heatmap.py
@@ -10,13 +10,20 @@ from folium.plugins import HeatMap
 
 
 
-
 map = folium.Map(location=[39.95700996945959, -75.15569443929508], zoom_start = 9.5)
 
+complete = open('complete.csv','r')
+data = complete.readlines()
+latlong = []
+for line in data:
+    datalist = line.split(",")
+    if datalist[0] == 'fibs code':
+        pass
+    else:
+        latlong = [float(datalist[2]), float(datalist[3])]
+        print(latlong)
+        HeatMap(latlong).add_to(map)
 
-
-lats_longs = [ [39.95700996945959,-75.15569443929508] ]
-
-HeatMap(lats_longs).add_to(map)
+# lats_longs = [ [39.95700996945959,-75.15569443929508] ]
 map.save('map.html')
-webbrowser.open('map.html')
+# webbrowser.open('map.html')
