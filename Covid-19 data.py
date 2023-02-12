837dc19362d76301587c5b85a072ee0753a89568
Michael Zavi <99049169+vareq1@users.noreply.github.com>
2023-02-11 20:11:18 -0500

    organize data final

--- Covid-19 data.py
+++ Covid-19 data.py
@@ -14,6 +14,7 @@ countiesFile = open('counties.csv', 'r')
 countiesLines = countiesFile.readlines()
 countiesList = []
 countiesFIPS = []
+countyRisk = {}
 
 # Append counties and FIPS codes to lists
 for line in countiesLines:
@@ -21,16 +22,10 @@ for line in countiesLines:
     if linelist[2] == "PA":
         countiesList.append(linelist[3])
         countiesFIPS.append(linelist[6][24:29:])
+        countyRisk[linelist[6][24:29]] = linelist[18]
 
-#Create dictionary of County, long and lat
-
-countyRisk = {}
-
-for line in countiesLines:
-     linelist = line.split(',')
-     if linelist[2] == "PA":
-          
-          
+#print(countyRisk)
+#print(countiesFIPS)
 
 with open('us_county_latlng.csv' , 'r') as file:
     fileLines = file.readlines()
@@ -38,14 +33,20 @@ with open('us_county_latlng.csv' , 'r') as file:
     for line in fileLines:
         lineList = line.split(',')
         if lineList[0] in countiesFIPS:
+            code = lineList[0]
 
             #Append the jawns the match into this file
+            with open('complete.csv' , 'a') as final:
 
-            with open('complete.csv' , 'wb') as final:
                     writer = csv.writer(final)
-                    data = lineList[0],lineList[1],lineList[2],lineList[3][:-2:]
+                    data = lineList[0],lineList[1],lineList[2],lineList[3][:-2:],countyRisk[code]
+
+                    #print(countyRisk[code])
                     print(data)
+                    
                     writer.writerow(data)
+    else:
+         pass
 
 
 # Create csv with county name, longitude, latitude
