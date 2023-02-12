e5e4ca236d02c15be1d3a21e882bbca20e7de459
Nkorsah <Nkorsah21@gmail.com>
2023-02-11 19:21:59 -0500

    yeoo

--- Covid-19 data.py
+++ Covid-19 data.py
@@ -16,10 +16,17 @@ file = open('counties.csv', 'r')
 data = file.readlines()
 #print(data)
 counties = []
+# finds the counties in PA
+x = 0
 for line in data:
     stuff = line.split(",")
     states = stuff[2]
     if states == "PA":
         counties.append(stuff[3])
+        x += 1
 
 print(counties)
+print(x)
+# make a new list for the info for each county we need
+fileCounty = open('us_county_latlng.csv','r')
+
--- Heatmap.py
+++ Heatmap.py
@@ -1,7 +1,22 @@
 # CMD promp as administrator -> pip install Seaborn
+# https://youtu.be/d07MOXnNlyI heatmap vid
 
-import seaborn
-import numpy
-import matplotlib.pylab
+# import seaborn
+# import numpy
+# import matplotlib.pylab
+import folium
+import webbrowser
+from folium.plugins import HeatMap
 
 
+
+
+map = folium.Map(location=[39.95700996945959, -75.15569443929508], zoom_start = 9.5)
+
+
+
+lats_longs = [ [39.95700996945959,-75.15569443929508] ]
+
+HeatMap(lats_longs).add_to(map)
+map.save('map.html')
+webbrowser.open('map.html')
