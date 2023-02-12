74569e6795eb5b5e1119ae2f6ece7e1eb7ba172c
Nkorsah <Nkorsah21@gmail.com>
2023-02-11 21:52:57 -0500

    yo

--- Heatmap.py
+++ Heatmap.py
@@ -7,12 +7,14 @@
 import folium
 import webbrowser
 from folium.plugins import HeatMap
-
-
+from folium.features import GeoJsonTooltip
+import geopandas as gpd
+import pandas as pd
 
 map = folium.Map(location=[39.95700996945959, -75.15569443929508], zoom_start = 9.5)
 
 complete = open('complete.csv','r')
+dadata = pd.read_csv('complete.csv')
 data = complete.readlines()
 latlong = []
 cords = []
@@ -26,7 +28,21 @@ for line in data:
 
 for item in cords:
     pass
-HeatMap(cords, radius=50).add_to(map)
+folium.Choropleth(
+    geo_data="https://raw.githubusercontent.com/python-visualization/folium/main/examples/data/us-states.json",
+    name="choropleth",
+    data=dadata,
+    columns=["county", "long", "risk_level"],
+    key_on="feature.id",
+    fill_color="YlGn",
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name="risk_level",
+).add_to(map)
+
+folium.LayerControl().add_to(map)
+
+# HeatMap(cords, radius=50).add_to(map)
 
 
 map.save('map.html')
